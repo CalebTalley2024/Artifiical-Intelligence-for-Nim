e257d621a0544ff91ad87e60638a7c61b2d99efe
Caleb Talley <86822049+CalebTalley2024@users.noreply.github.com>
2022-10-26 00:08:01 -0400

    Added subtasks for  parts 2 and 3

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,31 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+// Author: Caleb Tall
+
+/* CreateMarkers
+* returns 3 arrays of markers
+*
+* */
+
+//
+
+// 1. FIND OUT ???????????????????????????
+
+/* 2 and 3
+- subtasks
+	- create 3 arrays of diff colored markers
+	- User
+		- picking color
+		- picking num of markers to take out
+		-
+	- CPU
+		- randomly pick color
+		- randomly pick num of markers to take out
+	- after each turn
+		- check to see if there is only one color remaining ( if 2 arrays are empty)
+
+4
+subtasks
+* */
+
